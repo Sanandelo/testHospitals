7c3e6cf7cd5e5d455470d401985e1371b90a9609
Sanandelo <radgastt@gmail.com>
2017-04-06 14:34:10 +0300

    v.2

--- src/main/java/com/example/tests/ManagePage.java
+++ src/main/java/com/example/tests/ManagePage.java
@@ -54,6 +54,9 @@ public class ManagePage extends PageObject{
     @FindBy(xpath = "//*[@id=\"allDoctors\"]/thead/tr/th[2]/i")
     private WebElement emailLabel;
 
+
+    //Header row in table
+
     @FindBy(id = "email")
     private WebElement sortByEmailButton;
 
@@ -75,6 +78,84 @@ public class ManagePage extends PageObject{
     @FindBy(id="specialization")
     private WebElement sortBySpecializationButton;
 
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/thead/tr/th[6]/i")
+    private WebElement categoryLabel;
+
+    @FindBy(id="category")
+    private WebElement getSortByCategoryButton;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/thead/tr/th[7]/i")
+    private WebElement actionLabel;
+
+    //Rows
+
+    //First row
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[1]")
+    private WebElement firstRowNumber;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[2]")
+    private WebElement firstRowEmail;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[3]")
+    private WebElement firstRowName;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[4]")
+    private WebElement firstRowSurname;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[5]")
+    private WebElement firstRowSpecialization;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[1]/td[6]")
+    private WebElement firstRowCategory;
+
+    @FindBy(xpath = "//table/tbody/tr[1]/td[7]/span[1]/a")
+    private WebElement firstRowShowDoctorsDetailButton;
+
+    @FindBy(xpath = "//table/tbody/tr[1]/td[7]/span[2]/a")
+    private WebElement firstRowEditDoctorsDetailButton;
+
+    @FindBy(xpath = "//table/tbody/tr[1]/td[7]/span[3]/a")
+    private WebElement firstRowShowSchedulerButton;
+
+    @FindBy(xpath = "//table/tbody/tr[1]/td[7]/span[4]/a")
+    private WebElement firstRowDeleteDoctorButton;
+
+    //Second Row
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[1]")
+    private WebElement secondRowNumber;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[2]")
+    private WebElement secondRowEmail;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[3]")
+    private WebElement secondRowName;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[4]")
+    private WebElement secondRowSurname;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[5]")
+    private WebElement secondRowSpecialization;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[6]")
+    private WebElement secondRowCategory;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[7]/span[1]/a")
+    private WebElement secondRowShowDoctorsDetailButton;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[7]/span[2]/a")
+    private WebElement secondRowEditDoctorsDetailButton;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[7]/span[3]/a")
+    private WebElement secondRowShowSchedulerButton;
+
+    @FindBy(xpath = "//*[@id=\"allDoctors\"]/tbody/tr[2]/td[7]/span[4]/a")
+    private WebElement secondRowDeleteDoctorButton;
+
+    @FindBy(xpath = "//html/body/section/a[2]")
+    private WebElement backToTopButton;
+
+    //Popups??
 
     public ManagePage(WebDriver driver) {
         super(driver);
